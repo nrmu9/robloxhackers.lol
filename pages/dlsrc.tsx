930952b76c39a3bde5c40d36a1d212d1acb14115
nnaem <kacperzoltowski4@gmail.com>
2024-06-08 18:57:33 +0200

    fixes

--- pages/dlsrc.tsx
+++ pages/dlsrc.tsx
@@ -23,13 +23,13 @@ const DownloadForm = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="p-6 bg-white rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold mb-4">Download MacSploit&apos;s Source</h1>
-        <p className="mb-6 text-gray-700">Click the button below to download MacSploit&apos;s source code.</p>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="p-6 rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-4 text-white">Download MacSploit&apos;s Source</h1>
+        <p className="mb-6 text-white">Click the button below to download MacSploit&apos;s source code.</p>
         <button 
           onClick={handleDownload} 
-          className="px-6 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50"
+          className="px-6 py-2 text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50"
         >
           Download
         </button>
