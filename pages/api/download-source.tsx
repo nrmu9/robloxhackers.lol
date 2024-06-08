52256d81aa97dbc8c80638ed8fbedce443b3a8ce
nnaem <kacperzoltowski4@gmail.com>
2024-06-08 18:52:24 +0200

    fixes

--- pages/api/download-source.tsx
+++ pages/api/download-source.tsx
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const validReferer = 'https://loot-link.com/s?9c330e9882c311a2'; // Change to the actual URL of the ad redirect link
+const validReferer = 'https://loot-link.com'; // Change to the actual URL of the ad redirect link
 
 export default function handler(req: { headers: { referer: any; }; }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; setHeader: (arg0: string, arg1: string) => void; send: (arg0: Buffer) => void; }) {
   const referer = req.headers.referer;
