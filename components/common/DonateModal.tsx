017064d673a80ca3329018fe513e84e5684cc454
nnaem <kacperzoltowski4@gmail.com>
2024-06-12 21:23:28 +0200

    Added crypto donations

--- components/common/DonateModal.tsx
+++ components/common/DonateModal.tsx
@@ -1,5 +1,5 @@
 // components/common/DonateModal.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from '@/components/common/Modal';
 import Image from 'next/image';
 
@@ -9,6 +9,32 @@ type DonateModalProps = {
 };
 
 const DonateModal: React.FC<DonateModalProps> = ({ isOpen, onClose }) => {
+  const cryptoDonations = [
+    { name: 'BTC', address: 'bc1q25l9ewkkewag0wz0srnjk9whr8u4hgyk2l8mk8' },
+    { name: 'ETH (ETH Network)', address: '0xa3095ecC94C75364cb9A0E1EE967Fbcb1C9B24B5' },
+    { name: 'LTC (LTC Network)', address: 'LN619o1C1q5aArLRMnsX7myTMaHjAT3sQ8' },
+    { name: 'XMR', address: '49n8RRi9zuGPvXjw6V5fHcPXqU528gKzrCgSPmbbKYDe7sKM7ekSghrhypamV5cRyLhMsUtHKybsr2CfSEPnLzBrMdQ3zAt' },
+    { name: 'USDT (ETH Network)', address: '0xa3095ecC94C75364cb9A0E1EE967Fbcb1C9B24B5' },
+  ];
+
+  const [copiedAddress, setCopiedAddress] = useState<boolean[]>(new Array(cryptoDonations.length).fill(false));
+
+  const copyToClipboard = (index: number, address: string) => {
+    navigator.clipboard.writeText(address);
+    setCopiedAddress(prevState => {
+      const newState = [...prevState];
+      newState[index] = true;
+      return newState;
+    });
+    setTimeout(() => {
+      setCopiedAddress(prevState => {
+        const newState = [...prevState];
+        newState[index] = false;
+        return newState;
+      });
+    }, 2000); // Change back after 2 seconds
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="text-center">
@@ -39,6 +65,21 @@ const DonateModal: React.FC<DonateModalProps> = ({ isOpen, onClose }) => {
           >
             Donate via Roblox
           </a>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-lg font-semibold mb-2">Crypto Donations</h3>
+          {cryptoDonations.map(({ name, address }, index) => (
+            <div key={name} className="flex items-center justify-between mb-2">
+              <span className="text-sm break-all flex-grow text-left">{name}: {address}</span>
+              <button
+                onClick={() => copyToClipboard(index, address)}
+                className="ml-2 text-blue-500 underline text-sm"
+                style={{ minWidth: '100px' }}
+              >
+                {copiedAddress[index] ? 'Copied!' : 'Copy Address'}
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </Modal>
