0ef5a72a67628fcce4fa9015b73ad01f4e003817
u/Failed_cocacola <123872886+localscripts@users.noreply.github.com>
2024-08-07 05:34:13 -0700

    Update discord.tsx

--- pages/discord.tsx
+++ pages/discord.tsx
@@ -5,7 +5,7 @@ const DiscordPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('https://discord.gg/V55rJUqQVT');
+    router.push('https://discord.gg/psmQQSr5fu');
   }, [router]);
 
   return null;
