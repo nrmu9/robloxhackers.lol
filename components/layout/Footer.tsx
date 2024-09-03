f600770a3d6bfff0dcd6b74321ad0c3d3f29508f
nrmu <kacperzoltowski4@gmail.com>
2024-09-03 16:39:32 +0200

    add exploit status, fix footer and meta tags

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
@@ -5,7 +6,7 @@ const Footer = () => {
     <footer className="bg-black border-t-2 border-zinc-800 py-4 text-white text-center user-select-none">
       <div className="container mx-auto">
         <p className="text-lg font-semibold">&copy; {new Date().getFullYear()} voxlis.NET. All rights reserved.</p>
-        <p className="text-sm mt-2">Join our community on <a href="/discord" className="text-indigo-600 hover:text-indigo-400 transition-colors duration-200">Discord</a></p>
+        <p className="text-sm mt-2">Join our community on <Link href="/discord" className="text-red-500 hover:text-red-400 transition-colors duration-200">Discord</Link></p>
       </div>
     </footer>
   );
